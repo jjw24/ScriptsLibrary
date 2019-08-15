0864d025c500c368df332cedd6bfe88394393a21
Jeremy Wu <jeremy24wu@gmail.com>
2019-08-16 08:05:34 +1000

    Add file linking settings

---
+++ Wox/Plugins/Wox.Plugin.ScriptsLibrary/Models/FileLink.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+namespace Wox.Plugin.ScriptsLibrary.Models
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class FileLink
+    {
+        [JsonProperty]
+        public string Path { get; set; }
+
+        public string FileName =>
+           Path.Split(new[] { System.IO.Path.DirectorySeparatorChar }, StringSplitOptions.None)
+               .Last()
+           + " (" + System.IO.Path.GetDirectoryName(Path) + ")";
+    }
+}
--- Wox/Plugins/Wox.Plugin.ScriptsLibrary/Settings.cs
+++ Wox/Plugins/Wox.Plugin.ScriptsLibrary/Settings.cs
@@ -1,9 +1,15 @@
-﻿
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using Wox.Plugin.ScriptsLibrary.Models;
+
 namespace Wox.Plugin.ScriptsLibrary
 {
     public class Settings
     {
         public string Profile { get; set; }
         public string Region { get; set; }
+
+        [JsonProperty]
+        public List<FileLink> FolderLinks { get; set; } = new List<FileLink>();
     }
 }
