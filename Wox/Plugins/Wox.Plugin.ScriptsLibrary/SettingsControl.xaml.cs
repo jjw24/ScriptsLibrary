67dd4e10c2d93cd709c33e338991f9e70cb53b52
Jeremy Wu <jeremy24wu@gmail.com>
2019-08-27 06:59:06 +1000

    Add Ookii.Dialogs.Wpf

--- Wox/Plugins/Wox.Plugin.ScriptsLibrary/SettingsControl.xaml.cs
+++ Wox/Plugins/Wox.Plugin.ScriptsLibrary/SettingsControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,13 +14,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
-
+using Ookii.Dialogs.Wpf;
+using Wox.Plugin.ScriptsLibrary.Models;
 
 namespace Wox.Plugin.ScriptsLibrary
-{
-    /// <summary>
-    /// SettingsControl.xaml 的交互逻辑
-    /// </summary>
+{   
     public partial class SettingsControl : UserControl
     {
         private Settings _settings;
@@ -30,14 +29,77 @@ namespace Wox.Plugin.ScriptsLibrary
             InitializeComponent();
             _settings = settings;
             _woxAPI = woxAPI;
-            profileBox.Text = _settings.Profile;
-            regionBox.Text = _settings.Region;
+            //profileBox.Text = _settings.Profile;
+            //regionBox.Text = _settings.Region;
         }
 
         private void OnApplyBTClick(object sender, RoutedEventArgs e)
         {
-            _settings.Profile = profileBox.Text;
-            _settings.Region = regionBox.Text;
+            //_settings.Profile = profileBox.Text;
+            //_settings.Region = regionBox.Text;
+        }
+
+
+        private void btnAddFiles_Click(object sender, RoutedEventArgs e)
+        {
+            var fileBrowserDialog = new OpenFileDialog();
+            fileBrowserDialog.Multiselect = true;
+            if (fileBrowserDialog.ShowDialog() == true)
+            {
+                var newFolder = new FileLink
+                {
+                    Path = fileBrowserDialog.FileName
+                };
+
+                if (_settings.FolderLinks == null)
+                {
+                    _settings.FolderLinks = new List<FileLink>();
+                }
+
+                _settings.FolderLinks.Add(newFolder);
+            }
+
+            lbxFiles.Items.Refresh();
+        }
+
+        private void lbxFolders_Drop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (files != null && files.Count() > 0)
+            {
+                if (_settings.FolderLinks == null)
+                {
+                    _settings.FolderLinks = new List<FileLink>();
+                }
+
+                foreach (string s in files)
+                {
+                    if (Directory.Exists(s))
+                    {
+                        var newFolder = new FileLink
+                        {
+                            Path = s
+                        };
+
+                        _settings.FolderLinks.Add(newFolder);
+                    }
+
+                    lbxFiles.Items.Refresh();
+                }
+            }
+        }
+
+        private void lbxFolders_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
         }
     }
 }
