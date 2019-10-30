10e78a267073c3c580d8820d12e45e5a2370e206
Jeremy Wu <jeremy24wu@gmail.com>
2019-10-30 21:11:29 +1100

    Test update method names

--- Wox/ScriptsLibrary.Test/ParameterTests.cs
+++ Wox/ScriptsLibrary.Test/ParameterTests.cs
@@ -6,13 +6,21 @@ namespace ScriptsLibrary.Test
     [TestFixture]
     public class ParameterTests
     {
-        [TestCase("username;password;computer")]
-        public void WhenGivenParameterStringThenShouldSplitStringsToListWithoutSeperator(string parameterString)
+        [TestCase("blah.bat -p username;password;computer")]
+        public void WhenGivenQueryStringThenShouldReturnParametersOnly(string queryString)
         {
-            var result = Parameter.SplitString(parameterString);
+            var result = Parameter.GetParametersFromQuery(queryString);
 
-            Assert.True(result.Count == 3);
-            Assert.True(result[0] + ";"+ result[1] + ";"+ result[2] == parameterString);
+            //Assert.True(result.Count == 3);
+            Assert.True(result== "username password computer");
+        }
+
+        [TestCase("blah.bat -p username;", 0)]
+        public void WhenGivenQueryStringWithInsufficientParametersThenShouldReturnExpectedNumber(string queryString, int expectedBoolResult)
+        {
+            var result = queryString.GetParametersFromQueryCount();
+
+            Assert.True(result == expectedBoolResult);
         }
 
         [TestCase("username;password;computer", true)]
