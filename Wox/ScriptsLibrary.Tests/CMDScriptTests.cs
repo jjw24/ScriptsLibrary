47f20194e02125444cabffb6003d17cf6fd95ae0
Jeremy Wu <jeremy24wu@gmail.com>
2019-10-30 21:57:35 +1100

    Fix Test project error due to incorrect .net framework

---
+++ Wox/ScriptsLibrary.Tests/CMDScriptTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Wox.Plugin.ScriptsLibrary.Commands;
+
+namespace ScriptsLibrary.Tests
+{
+    [TestFixture]
+    public class CMDScriptTests
+    {
+        [TestCase]
+        public void test()
+        {
+            CMDScript.RunCMDWithParameters("C:\\Users\\JJW\\Desktop\\Hello.bat", "JWU Blah");
+        }
+    }
+}
---
+++ Wox/ScriptsLibrary.Tests/ParameterTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using Wox.Plugin.ScriptsLibrary.Commands;
+
+namespace ScriptsLibrary.Tests
+{
+    [TestFixture]
+    public class ParameterTests
+    {
+        [TestCase("blah.bat -p username;password;computer")]
+        public void WhenGivenQueryStringThenShouldReturnParametersOnly(string queryString)
+        {
+            var result = Parameter.GetParametersFromQuery(queryString);
+
+            //Assert.True(result.Count == 3);
+            Assert.True(result == "username password computer");
+        }
+
+        [TestCase("blah.bat -p username;", 0)]
+        public void WhenGivenQueryStringWithInsufficientParametersThenShouldReturnExpectedNumber(string queryString, int expectedBoolResult)
+        {
+            var result = queryString.GetParametersFromQueryCount();
+
+            Assert.True(result == expectedBoolResult);
+        }
+
+        [TestCase("username;password;computer", true)]
+        [TestCase(";username;password;computer", false)]
+        [TestCase(";", false)]
+        [TestCase("username;password;computer;", false)]
+        [TestCase(" ", false)]
+        [TestCase("", true)]
+        public void WhenGivenParameterStringThenShouldReturnExpectedBoolResult(string parameterString, bool expectedBoolResult)
+        {
+            var result = Parameter.ValidateString(parameterString);
+
+            Assert.True(result == expectedBoolResult);
+        }
+    }
+}
